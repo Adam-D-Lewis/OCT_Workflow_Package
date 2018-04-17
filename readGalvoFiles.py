457da2687e7fce811d2acd500edc737af14ea2f9
Adam Lewis <balast@users.noreply.github.com>
2018-04-17 13:52:05 -0500

    Added Files

---
+++ readGalvoFiles.py
@@ -0,0 +1,15 @@
+import numpy as np
+def readGalvoFiles(filename):
+    segSize = 12500
+    data = np.fromfile(filename, dtype='>d')
+    if np.size(data) % 2*segSize == 0:
+        numIter = int(np.size(data)/segSize)
+        xGalvo, yGalvo = [], []
+        for i in range(numIter):
+            xGalvo = np.append(xGalvo, data[2*i*segSize:(2*i+1)*segSize])
+            yGalvo = np.append(yGalvo, data[(2*i+1)*segSize:(2*i+2)*segSize])
+        return [xGalvo, yGalvo]
+    else:
+        raise ValueError('The filename size isnt divisible by ' + str(2*segSize))
+
+    print('bye')
---
+++ volt_to_pos.py
@@ -0,0 +1,4 @@
+import numpy as np
+def voltage_to_pos(xy, voltage):
+    x_mean
+    return position
